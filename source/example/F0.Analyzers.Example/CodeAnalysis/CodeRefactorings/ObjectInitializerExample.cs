286f430e30cd08027d2d9c1a78dad7dca47e0110
Flash0ver <38893694+Flash0ver@users.noreply.github.com>
2020-05-06 09:47:51 +0200

    add more examples
    * type from same file
    * type from same project
    * type from other project
    * type from package

--- source/example/F0.Analyzers.Example/CodeAnalysis/CodeRefactorings/ObjectInitializerExample.cs
+++ source/example/F0.Analyzers.Example/CodeAnalysis/CodeRefactorings/ObjectInitializerExample.cs
@@ -1,20 +1,58 @@
-﻿namespace F0.Analyzers.Example.CodeAnalysis.CodeRefactorings
+﻿using System;
+using F0.Analyzers.Example.Dependencies.Dependency;
+using F0.Analyzers.Example.Shared;
+using Microsoft.Extensions.Hosting;
+
+namespace F0.Analyzers.Example.CodeAnalysis.CodeRefactorings
 {
-	internal sealed class Model
-	{
-		public int Number { get; set; }
-
-		public string Text { get; set; }
-	}
-
 	internal sealed class ObjectInitializerExample
 	{
-		public Model GetModel()
+		public ValueTuple<bool, int, string> GetBclType()
+		{
+			var tuple = new ValueTuple<bool, int, string>();
+			return tuple;
+		}
+
+		public Nested GetNestedType()
+		{
+			return new Nested();
+		}
+
+		public Model GetTypeFromSameAssembly()
 		{
 			var model = new Model();
-
-
 			return model;
+		}
+
+		public PublicPoco GetPublicTypeFromOtherAssembly()
+		{
+			var poco = new PublicPoco();
+			return poco;
+		}
+
+		public InternalPoco GetInternalTypeFromFriendAssembly()
+		{
+			var poco = new InternalPoco();
+			return poco;
+		}
+
+		public ConsoleLifetimeOptions GetTypeFromPackage()
+		{
+			var options = new ConsoleLifetimeOptions();
+			return options;
+		}
+
+		internal struct Nested
+		{
+			public int Field;
+
+			public int Property { get; set; }
+
+			public void Reset()
+			{
+				Field = 0;
+				Property = 0;
+			}
 		}
 	}
 }
---
+++ source/example/F0.Analyzers.Example/Shared/Model.cs
@@ -0,0 +1,11 @@
+﻿namespace F0.Analyzers.Example.Shared
+{
+	internal sealed class Model
+	{
+		public bool Condition { get; set; }
+
+		public int Number { get; set; }
+
+		public string Text { get; set; }
+	}
+}
