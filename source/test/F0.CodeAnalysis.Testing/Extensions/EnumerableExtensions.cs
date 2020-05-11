37d6dce129a2c36e3868fd88d09d8509d7a71ba9
Flash0ver <38893694+Flash0ver@users.noreply.github.com>
2020-05-11 09:44:08 +0200

    add copyright notice to Roslyn-SDK samples code

--- source/test/F0.CodeAnalysis.Testing/Extensions/EnumerableExtensions.cs
+++ source/test/F0.CodeAnalysis.Testing/Extensions/EnumerableExtensions.cs
@@ -1,11 +1,12 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+// https://github.com/dotnet/roslyn-sdk
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace F0.Testing.Extensions
 {
-	internal static partial class EnumerableExtensions
+	internal static class EnumerableExtensions
 	{
 		public static IEnumerable<T> OrderBy<T>(this IEnumerable<T> source, IComparer<T> comparer)
 		{
