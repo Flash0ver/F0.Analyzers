32f1e44c23fd32c70a1e1e91b03b5b5349fc70c7
Flash0ver <38893694+Flash0ver@users.noreply.github.com>
2020-06-22 02:26:57 +0200

    refactor: mark assembly with NeutralResourcesLanguageAttribute

---
+++ source/test/F0.Analyzers.Tests/AssemblyInfoTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using Xunit;
+
+namespace F0.Tests
+{
+	public class AssemblyInfoTests
+	{
+		[Fact]
+		public void AssemblyInfo_DefaultCulture_English()
+		{
+			var version = new Version(0, 3, 0, 0);
+			var displayName = $"F0.Analyzers, Version={version}, Culture=neutral, PublicKeyToken=null";
+
+			var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+			var assembly = assemblies.Single(a => a.FullName.Equals(displayName, StringComparison.InvariantCulture));
+
+			var attribute = assembly.GetCustomAttribute<NeutralResourcesLanguageAttribute>();
+
+			Assert.NotNull(attribute);
+			Assert.Equal("en", attribute.CultureName);
+			Assert.Equal(UltimateResourceFallbackLocation.MainAssembly, attribute.Location);
+		}
+	}
+}
