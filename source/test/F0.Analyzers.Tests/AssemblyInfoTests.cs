a7283761cedbe5c838709d0d19f37099d5709f63
Flash0ver <38893694+Flash0ver@users.noreply.github.com>
2020-06-23 19:24:46 +0200

    test: remove code duplication

--- source/test/F0.Analyzers.Tests/AssemblyInfoTests.cs
+++ source/test/F0.Analyzers.Tests/AssemblyInfoTests.cs
@@ -9,15 +9,12 @@ namespace F0.Tests
 {
 	public class AssemblyInfoTests
 	{
+		private static readonly Version version = new Version(0, 3, 0, 0);
+
 		[Fact]
 		public void AssemblyInfo_DefaultCulture_English()
 		{
-			var version = new Version(0, 3, 0, 0);
-			var displayName = $"F0.Analyzers, Version={version}, Culture=neutral, PublicKeyToken=null";
-
-			var assemblies = AppDomain.CurrentDomain.GetAssemblies();
-			var assembly = assemblies.Single(a => a.FullName.Equals(displayName, StringComparison.InvariantCulture));
-
+			var assembly = GetAnalyzerAssembly();
 			var attribute = assembly.GetCustomAttribute<NeutralResourcesLanguageAttribute>();
 
 			Assert.NotNull(attribute);
@@ -28,16 +25,21 @@ namespace F0.Tests
 		[Fact]
 		public void AssemblyInfo_AnalyzerAssemblyHasNeitherPubliclyExposedTypesNorMembers_TheAssemblyIsNotCLSCompliant()
 		{
-			var assemblies = AppDomain.CurrentDomain.GetAssemblies();
-
-			var version = new Version(0, 3, 0, 0);
-			var displayName = $"F0.Analyzers, Version={version}, Culture=neutral, PublicKeyToken=null";
-
-			var assembly = assemblies.Single(a => a.FullName.Equals(displayName, StringComparison.InvariantCulture));
+			var assembly = GetAnalyzerAssembly();
 			var attribute = assembly.GetCustomAttribute<CLSCompliantAttribute>();
 
 			Assert.NotNull(attribute);
 			Assert.False(attribute.IsCompliant);
+		}
+
+		private static Assembly GetAnalyzerAssembly()
+		{
+			var displayName = $"F0.Analyzers, Version={version}, Culture=neutral, PublicKeyToken=null";
+
+			var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+			var assembly = assemblies.Single(a => a.FullName.Equals(displayName, StringComparison.InvariantCulture));
+
+			return assembly;
 		}
 	}
 }
