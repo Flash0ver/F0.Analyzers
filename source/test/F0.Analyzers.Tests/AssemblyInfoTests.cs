1aab1223d5c73368ee0379eaa1a28a0e1d7b3e9b
Flash0ver <38893694+Flash0ver@users.noreply.github.com>
2020-06-25 11:47:10 +0200

    refactor: mark assembly with NeutralResourcesLanguageAttribute (#19)

--- source/test/F0.Analyzers.Tests/AssemblyInfoTests.cs
+++ source/test/F0.Analyzers.Tests/AssemblyInfoTests.cs
@@ -2,25 +2,44 @@
 using System;
 using System.Linq;
 using System.Reflection;
+using System.Resources;
 using Xunit;
 
 namespace F0.Tests
 {
 	public class AssemblyInfoTests
 	{
+		private static readonly Version version = new Version(0, 3, 0, 0);
+
+		[Fact]
+		public void AssemblyInfo_DefaultCulture_English()
+		{
+			var assembly = GetAnalyzerAssembly();
+			var attribute = assembly.GetCustomAttribute<NeutralResourcesLanguageAttribute>();
+
+			Assert.NotNull(attribute);
+			Assert.Equal("en", attribute.CultureName);
+			Assert.Equal(UltimateResourceFallbackLocation.MainAssembly, attribute.Location);
+		}
+
 		[Fact]
 		public void AssemblyInfo_AnalyzerAssemblyHasNeitherPubliclyExposedTypesNorMembers_TheAssemblyIsNotCLSCompliant()
 		{
-			var assemblies = AppDomain.CurrentDomain.GetAssemblies();
-
-			var version = new Version(0, 3, 0, 0);
-			var displayName = $"F0.Analyzers, Version={version}, Culture=neutral, PublicKeyToken=null";
-
-			var assembly = assemblies.Single(a => a.FullName.Equals(displayName, StringComparison.InvariantCulture));
+			var assembly = GetAnalyzerAssembly();
 			var attribute = assembly.GetCustomAttribute<CLSCompliantAttribute>();
 
 			Assert.NotNull(attribute);
 			Assert.False(attribute.IsCompliant);
+		}
+
+		private static Assembly GetAnalyzerAssembly()
+		{
+			var displayName = $"F0.Analyzers, Version={version}, Culture=neutral, PublicKeyToken=null";
+
+			var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+			var assembly = assemblies.Single(a => a.FullName.Equals(displayName, StringComparison.InvariantCulture));
+
+			return assembly;
 		}
 	}
 }
