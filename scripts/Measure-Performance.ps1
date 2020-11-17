c45618fb76813f9097fb752ea7724a55d8a4ed0c
Flash0ver <38893694+Flash0ver@users.noreply.github.com>
2020-11-17 23:11:04 +0100

    ci: setup mutation testing pipeline (#49)
    via Stryker.NET
    add benchmark and mutation scripts

---
+++ scripts/Measure-Performance.ps1
@@ -0,0 +1,25 @@
+#Requires -Version 6.0
+
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]$Filter
+)
+
+Set-StrictMode -Version 3.0
+$ErrorActionPreference = 'Stop'
+
+$RepositoryRootPath = (Get-Item -Path $PSScriptRoot).Parent
+$SolutionFile = Join-Path -Path $RepositoryRootPath -ChildPath 'source' -AdditionalChildPath 'F0.Analyzers.Core.slnf'
+$BenchmarkProjectFile = Join-Path -Path $RepositoryRootPath -ChildPath 'source' -AdditionalChildPath 'performance', 'F0.Analyzers.Benchmarks', 'F0.Analyzers.Benchmarks.csproj'
+
+$Framework = 'netcoreapp3.1'
+
+dotnet clean $SolutionFile
+
+if ($Filter) {
+    dotnet run --project $BenchmarkProjectFile --configuration Release --framework $Framework --filter $Filter
+}
+else {
+    dotnet run --project $BenchmarkProjectFile --configuration Release --framework $Framework
+}
---
+++ scripts/Test-Mutation.ps1
@@ -0,0 +1,31 @@
+#Requires -Version 6.0
+
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [switch]$OpenReport
+)
+
+Set-StrictMode -Version 3.0
+$ErrorActionPreference = 'Stop'
+
+$RepositoryRootPath = (Get-Item -Path $PSScriptRoot).Parent
+$SolutionFile = Join-Path -Path $RepositoryRootPath -ChildPath 'source' -AdditionalChildPath 'F0.Analyzers.sln'
+$SolutionFilterFile = Join-Path -Path $RepositoryRootPath -ChildPath 'source' -AdditionalChildPath 'F0.Analyzers.Core.slnf'
+$TestProjectFile = Join-Path -Path $RepositoryRootPath -ChildPath 'source' -AdditionalChildPath 'test', 'F0.Analyzers.Tests', 'F0.Analyzers.Tests.csproj'
+$NuGetConfigurationFile = Join-Path -Path $RepositoryRootPath -ChildPath 'source' -AdditionalChildPath 'nuget.config'
+$StrykerConfigurationFile = Join-Path -Path $RepositoryRootPath -ChildPath 'source' -AdditionalChildPath 'test', 'stryker-config.json'
+
+dotnet clean $SolutionFilterFile
+
+dotnet tool restore --configfile $NuGetConfigurationFile
+
+$TestProjectFile = $($TestProjectFile.Replace('\', '/'))
+dotnet tool run dotnet-stryker --solution-path $SolutionFile --test-projects "['$TestProjectFile']" --config-file-path $StrykerConfigurationFile
+
+if ($OpenReport) {
+    $ResultsDirectory = Join-Path -Path $PSScriptRoot -ChildPath 'StrykerOutput'
+    $LatestReportDirectory = Get-ChildItem -Path $ResultsDirectory -Directory | Sort-Object -Property Name -Descending | Select-Object -First 1
+    $ReportFile = Join-Path -Path $LatestReportDirectory -ChildPath 'reports' -AdditionalChildPath 'mutation-report.html'
+    Invoke-Item -Path $ReportFile
+}
