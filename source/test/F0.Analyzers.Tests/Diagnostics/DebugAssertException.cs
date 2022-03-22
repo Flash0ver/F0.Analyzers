8b574964afaa876aec1eefa99acab7b36bdf7701
Flash0ver <38893694+Flash0ver@users.noreply.github.com>
2022-03-22 23:54:29 +0100

    test: suppress CA1064

--- source/test/F0.Analyzers.Tests/Diagnostics/DebugAssertException.cs
+++ source/test/F0.Analyzers.Tests/Diagnostics/DebugAssertException.cs
@@ -4,6 +4,7 @@ using System.Diagnostics.CodeAnalysis;
 namespace F0.Tests.Diagnostics;
 
 [SuppressMessage("Design", "CA1032:Implement standard exception constructors", Justification = "Replace Assertion Failed Dialog Box")]
+[SuppressMessage("Design", "CA1064:Exceptions should be public", Justification = "Replace Assertion Failed Dialog Box")]
 internal sealed class DebugAssertException : Exception
 {
 	public DebugAssertException(string? message)
