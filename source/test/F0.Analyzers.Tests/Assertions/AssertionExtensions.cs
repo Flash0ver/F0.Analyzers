fb176b0f9f3c9c928a96c704598ff35c7eeb1d64
Flash0ver <38893694+Flash0ver@users.noreply.github.com>
2024-01-22 23:55:44 +0100

    test: remove redundant Debug.Assert

--- source/test/F0.Analyzers.Tests/Assertions/AssertionExtensions.cs
+++ source/test/F0.Analyzers.Tests/Assertions/AssertionExtensions.cs
@@ -1,14 +1,12 @@
-using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 
 namespace F0.Tests.Assertions;
 
 internal static class AssertionExtensions
 {
+#pragma warning disable CS8777 // Parameter must have a non-null value when exiting.
 	internal static void ShouldNotBeNull<T>([NotNull] this T reference, string because = "", params object[] becauseArgs)
 		where T : class?
-	{
-		_ = reference.Should().NotBeNull(because, becauseArgs);
-		Debug.Assert(reference is not null, "Unreachable");
-	}
+		=> _ = reference.Should().NotBeNull(because, becauseArgs);
+#pragma warning restore CS8777 // Parameter must have a non-null value when exiting.
 }
